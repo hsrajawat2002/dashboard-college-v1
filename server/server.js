1584da653ed2fbc0d125649fc531ba37dc8d993a
Harshvardhan Singh <harshvardhan.singh.rajawat26@gmail.com>
2023-08-30 21:26:03 +0530

    changes in server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const server = require('http').createServer(app);
 const io = new Server(server , { 
     cors : {
         origin : "http://localhost:3000",
-        methods:["GET","POST"]
+        // credentials:true
     }
 })
 
